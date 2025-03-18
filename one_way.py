0d7aa277fb1382fef0dc4dafc257e26956b099c2
= <=>
2025-03-18 19:36:52 +0300

    one way up to the top of stairs if only restricted to a certain number of steps at a time

--- one_way.py
+++ one_way.py
@@ -9,5 +9,23 @@ def one_way_stairs(no_stairs: int, steps: List[int])-> List[int]:
     Returns:
         List[int]: _A list of combination of steps up the stairs_
     """
-    one_way_table: List[int] = [None for _ in range(no_stairs)]
+    one_way_table: List[int] = [None for _ in range(no_stairs  + 1)]
+    one_way_table[0] = []
+    one_way_table[1] = [1]
+    for index in range(1, no_stairs + 1):
+        if one_way_table[index] != None:
+            for step in steps:
+                if index + step < len(one_way_table):
+                     new =one_way_table[index][:]
+                     new.append(step)
+                     one_way_table[index + step] = new
     return one_way_table[no_stairs]
+
+def main()-> None:
+    steps: List[int] = [1,2]
+    height: int = 5
+    one_way: List[int] = one_way_stairs(no_stairs= height, steps= steps)
+    print(f"One way up a stairs of height {height}, given the steps to take at a time can be either of {steps} is {one_way}")
+
+if __name__ =="__main__":
+    main()
